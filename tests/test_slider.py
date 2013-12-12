dc4e25a41113e969688a8ba66005a9103af2741f
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-12-12 19:31:09 +0100

    Adds test case for slider.

---
+++ tests/test_slider.py
@@ -0,0 +1,30 @@
+from .setup import TestPygletGUI
+
+from pyglet_gui.sliders import HorizontalSlider
+from pyglet_gui.dialog import Dialog
+
+
+class TestSlider(TestPygletGUI):
+
+    def setUp(self):
+        TestPygletGUI.setUp(self)
+        self.slider = HorizontalSlider(min_value=0, max_value=10)
+        self.dialog = Dialog(self.slider, window=self.window, batch=self.batch, theme=self.theme)
+
+    def test_set_value(self):
+        self.slider.set_knob_pos(0.5)
+        self.assertEqual(self.slider.get_value(), 5)
+
+    def test_mouse_slide(self):
+
+        # push the slider to the minimum
+        self.slider.on_mouse_press(0, 0, None, None)
+        self.assertEqual(self.slider.get_value(), 0)
+
+        # push the slider to the maximum
+        self.slider.on_mouse_press(10000, 0, None, None)
+        self.assertEqual(self.slider.get_value(), 10)
+
+    def tearDown(self):
+        self.dialog.delete()
+        super().tearDown()
