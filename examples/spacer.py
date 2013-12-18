ae85483333ebee77e0e94721f5186fad043f5945
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-12-18 20:15:39 +0100

    Added example for usage of spacers.

---
+++ examples/spacer.py
@@ -0,0 +1,61 @@
+from setup import *
+
+from pyglet_gui.buttons import Button
+from pyglet_gui.dialog import Dialog
+from pyglet_gui.containers import GridLayout, HorizontalLayout, VerticalLayout
+from pyglet_gui.widgets import Spacer
+from pyglet_gui.constants import ANCHOR_CENTER
+from pyglet_gui.theme import Theme
+
+theme = Theme({"font": "Lucida Grande",
+               "font_size": 12,
+               "text_color": [255, 255, 255, 255],
+               "gui_color": [255, 0, 0, 255],
+               "button": {
+                   "down": {
+                       "image": {
+                           "source": "button-down.png",
+                           "frame": [8, 6, 2, 2],
+                           "padding": [18, 18, 8, 6]
+                       },
+                       "text_color": [0, 0, 0, 255]
+                   },
+                   "up": {
+                       "image": {
+                           "source": "button.png",
+                           "frame": [6, 5, 6, 3],
+                           "padding": [18, 18, 8, 6]
+                       }
+                   }
+               },
+               "checkbox": {
+                   "checked": {
+                       "image": {
+                           "source": "checkbox-checked.png"
+                       }
+                   },
+                   "unchecked": {
+                       "image": {
+                           "source": "checkbox.png"
+                       }
+                   }
+               }
+              }, resources_path='../theme/')
+
+# First line has two big buttons
+# second line has three spacers, separated by two small buttons.
+# size of the three spacers is the same.
+dialog = Dialog(VerticalLayout([HorizontalLayout([Button(label="Big fat button"),
+                                                  Button(label="Big fat button")], padding=0),
+
+                                HorizontalLayout([Spacer(),
+                                                  Button(label="Small"),
+                                                  Spacer(),
+                                                  Button(label="Small"),
+                                                  Spacer()], padding=0)],
+                               padding=0),
+                window=window,
+                batch=batch,
+                theme=theme)
+
+pyglet.app.run()
