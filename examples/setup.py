7dfdc99590e26d4d4fdebfeb2ccdbf4a16b58e54
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-12-21 18:26:39 +0100

    Removed unused code from example's setup.

--- examples/setup.py
+++ examples/setup.py
@@ -4,11 +4,8 @@ sys.path.insert(0, os.path.abspath('../'))
 
 import pyglet
 
-
 window = pyglet.window.Window(640, 480, resizable=True, vsync=True)
 batch = pyglet.graphics.Batch()
-bg_group = pyglet.graphics.OrderedGroup(0)
-fg_group = pyglet.graphics.OrderedGroup(1)
 
 @window.event
 def on_draw():
