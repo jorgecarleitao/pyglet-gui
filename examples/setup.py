19c06467d7b1108a17f212619d8e894fcd6ba309
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-12-21 09:12:37 +0100

    Removed "on_update" event and fps counter from examples.

--- examples/setup.py
+++ examples/setup.py
@@ -9,18 +9,8 @@ window = pyglet.window.Window(640, 480, resizable=True, vsync=True)
 batch = pyglet.graphics.Batch()
 bg_group = pyglet.graphics.OrderedGroup(0)
 fg_group = pyglet.graphics.OrderedGroup(1)
-fps = pyglet.clock.ClockDisplay()
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-    fps.draw()
-
-# register event to update pyglet-gui
-window.register_event_type('on_update')
-
-
-def update(dt):
-    window.dispatch_event('on_update', dt)
-pyglet.clock.schedule(update)
