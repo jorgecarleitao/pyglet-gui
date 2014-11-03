c89f1d76d36fdcdb2b8d5adaa5431d9310d31aa5
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-11-03 12:13:53 +0100

    Improved setup.py.
    - Fixed issue #12;
    - Fixed name;
    - Added classifiers.

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+# encoding: utf-8
 from distutils.core import setup
 
-setup(name='pyglet-gui',
+setup(name='Pyglet-gui',
       version='0.1',
       description='An extension of pyglet for GUIs',
+      long_description=open('README.md').read(),
       author='Jorge C. Leitão',
       url='https://github.com/jorgecarleitao/pyglet-gui',
-      packages=['pyglet_gui', 'pyglet_gui.theme'],
-      requires=['pyglet (>=1.2)']
+      packages=('pyglet_gui', 'pyglet_gui.theme'),
+      requires=('pyglet (>=1.2)',),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: MacOS X',
+          'Environment :: Win32 (MS Windows)',
+          'Environment :: X11 Applications',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Topic :: Games/Entertainment',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
 )
